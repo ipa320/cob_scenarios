497226edf737cce0a9d90367a090315620680952
Tobias Sing <tys@ipa.fhg.de>
2012-05-24 14:47:07 +0200

    new2

---
+++ ehealth2012/states/SMeHealth2012b.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+import roslib
+roslib.load_manifest('ehealth2012')   # todo: additional command line parameter
+import rospy
+import smach
+import smach_ros
+
+from generic_navigation_states import *
+from generic_perception_states import *
+
+from simple_script_server import *  # import script
+
+from PrepareRobot import *
+from SelectObjectFromKeyboard import *
+from Grasp import *
+
+class SMeHealth2012b(smach.StateMachine):
+    def __init__(self):
+        smach.StateMachine.__init__(self, outcomes=['ended'])
+        with self:
+            smach.StateMachine.add('PREPAREROBOT', PrepareRobot(),
+                                   transitions={
+                                    'succeeded':'SELECTOBJECTFROMKEYBOARD'
+                                   })
+            smach.StateMachine.add('SELECTOBJECTFROMKEYBOARD', SelectObjectFromKeyboard(),
+                                   transitions={
+                                    'objectSelected':'MOVE_TO_PREGRASP_POSITION',
+                                    'quit':'ended'
+                                  })
+            smach.StateMachine.add('MOVE_TO_PREGRASP_POSITION', approach_pose([1,0,3.14],mode="omni"),   
+                        transitions={'succeeded':'MOVE_TO_GRASP_POSITION', 
+                                                'failed':'ended'})
+                                   
+            smach.StateMachine.add('MOVE_TO_GRASP_POSITION', approach_pose([1.5,0,3.14],mode="linear"),
+                        transitions={'succeeded':'DETECT_OBJECT', 
+                                                'failed':'ended'})
+                                                
+            smach.StateMachine.add('DETECT_OBJECT', detect_object(max_retries = 5),
+                        transitions={'succeeded':'GRASP', 
+                                                'no_object':'DETECT_OBJECT',    #no_object -> retry DETECT_OBJECT
+                                                'no_more_retries':'ended', # object_not_picked
+                                                'failed':'ended'})  # failed
+
+            smach.StateMachine.add('GRASP', Grasp(),
+                                   transitions={
+                                    'grasped':'ended',
+                                    'not_grasped':'ended', 
+                                    'failed':'ended'
+                                   })
+                                          
+                                   
---
+++ ehealth2012/states/ehealth2012.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import roslib
+roslib.load_manifest('ehealth2012')
+import rospy
+import smach
+import smach_ros
+# from SMeHealth2 import *
+from simple_script_server import *  # import script
+sss = simple_script_server()
+
+from SMeHealth2012b import *
+
+def main():
+    rospy.init_node('eHealth2012')
+    #sm = smach.StateMachine(outcomes=['ended'])
+    #with sm:
+    #    smach.StateMachine.add('FOO', HierSM1(),
+    #                          transitions={'succeeded1':'ended'})
+    
+    sm = SMeHealth2012b()
+    outcome = sm.execute()
+    
+if __name__=='__main__':
+    main()
+   
